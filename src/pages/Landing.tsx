ada66c992ad47d38f318dd1473e87de7e6c365f8
Okoye <okoyedivine145@gmail.com>
2024-03-25 16:29:33 +0100

    Why Choose Us Section

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,6 +5,9 @@ import { RiMenu4Line } from "react-icons/ri";
 import clipper from "../assets/clipper.jpeg";
 import chairArtisan from "../assets/chairArtisan.jpeg";
 import nails from "../assets/nails.jpeg";
+import shipping from "../assets/svgs/shipping.svg";
+import variety from "../assets/svgs/variety.svg";
+import quality from "../assets/svgs/quality.svg";
 
 const Landing: FC = () => {
   const [loading, setLoading] = useState(true);
@@ -13,7 +16,6 @@ const Landing: FC = () => {
   }, []);
 
   const [shadow, setShadow] = useState(false);
-
   useEffect(() => {
     window.onscroll = () => {
       if (window.scrollY > 30) {
@@ -22,7 +24,35 @@ const Landing: FC = () => {
         setShadow(false);
       }
     };
-  }, [])
+  }, []);
+
+  interface whyUseArtisan {
+    header: string;
+    content: string;
+    icon: string;
+    id?: number;
+  }
+  const whyUseArtisanList: whyUseArtisan[] = [
+    {
+      header: "Convenience",
+      content:
+        "Our app offers hassle-free booking in just a few taps. Say goodbye to long phone calls and waiting times. With instant confirmation and easy rescheduling, managing your appointments has never been easier.",
+      icon: shipping,
+    },
+    {
+      header: "Variety",
+      content:
+        "Explore a diverse range of skilled artisans at your fingertips. Whether you need a stylist, makeup artist, or just a quick haircut, our platform connects you with talented professionals tailored to your specific needs and preferences.",
+      icon: variety,
+    },
+    {
+      header: "Quality Assurance",
+      content:
+        "Rest assured knowing that all artisans on our platform are thoroughly vetted and reviewed by previous clients. Experience top-notch service and impeccable craftsmanship every time you book through our app",
+      icon: quality,
+      id: 0,
+    },
+  ];
 
   return (
     <div className="overflow-x-hidden">
@@ -32,17 +62,21 @@ const Landing: FC = () => {
           !loading && "opacity-100"
         } `}
       >
-        <header className={`transition-all duration-300 ease-in-out px-2 py-1 landing-fade flex justify-between items-center overflow-hidden fixed w-full bg-white z-[9998] top-0 ${shadow && 'shadow-lg'}`}>
-          <img src={logo} alt="logo" className="w-20 z-10" />
-          <div className="p-4 rounded-full cursor-pointer z-10">
-            <RiMenu4Line size={"2rem"} />
+        <header
+          className={`transition-all duration-300 ease-in-out px-2 py-1 landing-fade flex justify-between items-center overflow-hidden fixed w-full bg-gradient-to-r from-white to-white/70 z-[9998] top-0 backdrop-blur-md ${
+            shadow && "shadow-lg"
+          }`}
+        >
+          <img src={logo} alt="logo" className="w-20 z-10 p-2" />
+          <div className="p-6 rounded-full cursor-pointer z-10">
+            <RiMenu4Line size={"1.5rem"} />
           </div>
         </header>
         <div className="flex justify-center relative mt-36">
           <span className="z-30">
             <img
               src={clipper}
-              alt=""
+              alt="clippers"
               className="border-2 h-[13rem] rounded-tr-[3rem] rounded-bl-[3rem] object-cover translate-x-4"
             />
           </span>
@@ -50,14 +84,14 @@ const Landing: FC = () => {
             <span>
               <img
                 src={chairArtisan}
-                alt=""
+                alt="chairArtisan"
                 className="border-2 rounded-tl-[1rem] rounded-br-[1rem] object-cover h-[7rem] z-10 -translate-y-7"
               />
             </span>
             <span className="z-20">
               <img
                 src={nails}
-                alt=""
+                alt="nails"
                 className="border-2 rounded-tr-[1rem] rounded-bl-[1rem] object-cover h-[10rem]"
               />
             </span>
@@ -89,9 +123,35 @@ const Landing: FC = () => {
             </div>
           </section>
         </div>
-        <section className="rounded-t-[2rem] bg-blue my-20 p-6 flex justify-center items-center z-20">
-          <h1 className="font-bold text-3xl text-white">Why use artisan?</h1>
+        <section className="rounded-t-[2rem] bg-gradient-to-b from-blue to-purple-700 to-[80%]  mt-20 p-6 flex flex-col justify-center items-center z-20">
+          <h1 className="font-bold text-3xl text-white mb-10">
+            Why use artisan?
+          </h1>
+          <div className="m-2 gap-10 flex flex-col justify-center items-center">
+            {whyUseArtisanList.map((obj, i) => (
+              <div
+                key={i}
+                className="text-white flex flex-col justify-center items-center gap-2"
+              >
+                <img
+                  src={obj.icon}
+                  alt={obj.header}
+                  className={`w-10 bg-white rounded-full ${
+                    obj.id == 0 ? " p-0" : "p-2"
+                  }`}
+                />
+                <h1 className="text-center text-2xl font-medium kaushan">
+                  {obj.header}
+                </h1>
+                <p className="text-xs w-3/4 text-center">{obj.content}</p>
+                {obj.id !== 0 && (
+                  <div className="mt-10 w-full h-[1px] bg-white"></div>
+                )}
+              </div>
+            ))}
+          </div>
         </section>
+        <section className="rounded-t-[2rem] flex justify-center items-center mt-20 p-6 z-20">download here, or try online</section>
       </div>
     </div>
   );
